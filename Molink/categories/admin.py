ac58af610fe388b668b51a010e1d0ac5eac9cf61
leemirim <leemirim9654@gmail.com>
2019-07-12 00:24:17 +0900

    마이그레이션 및 categories앱 어드민 등록

--- Molink/categories/admin.py
+++ Molink/categories/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'img_url',
+    )
