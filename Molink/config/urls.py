2f0e21dc0fa0d7dd694ca7b736d29d7cf85cea3b
leemirim <leemirim9654@gmail.com>
2019-07-12 00:28:00 +0900

    config/urls.py에 categories url 추가

--- Molink/config/urls.py
+++ Molink/config/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('/api/v1/folders/', include('folders.urls', namespace='folders')),
     path('/api/v1/links/', include('links.urls', namespace='links')),
     path('/api/v1/users/', include('users.urls', namespace='users')),
+    path('/api/v1/categories', include('categories.urls', namespace='categories')),
 ]
