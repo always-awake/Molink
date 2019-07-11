07ac7026189abe610792a9cb81584e07519387ca
leemirim <leemirim9654@gmail.com>
2019-07-12 00:36:06 +0900

    config/urls.py

--- Molink/config/urls.py
+++ Molink/config/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('/api/v1/admin/', admin.site.urls),
-    path('/api/v1/folders/', include('folders.urls', namespace='folders')),
-    path('/api/v1/links/', include('links.urls', namespace='links')),
-    path('/api/v1/users/', include('users.urls', namespace='users')),
-    path('/api/v1/categories', include('categories.urls', namespace='categories')),
+    path('api/v1/admin/', admin.site.urls),
+    path('api/v1/folders/', include('folders.urls', namespace='folders')),
+    path('api/v1/links/', include('links.urls', namespace='links')),
+    path('api/v1/users/', include('users.urls', namespace='users')),
+    path('api/v1/categories', include('categories.urls', namespace='categories')),
 ]
