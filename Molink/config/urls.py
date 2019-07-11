11f565720b81e3f465802616ca7ced389b885ead
leemirim <leemirim9654@gmail.com>
2019-07-11 22:43:28 +0900

    links, users url 설정

--- Molink/config/urls.py
+++ Molink/config/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('folders/', include('folders.urls', namespace='folders')),
+    path('links/', include('links.urls', namespace='links')),
+    path('users/', include('users.urls', namespace='users')),
 ]
