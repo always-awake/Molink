59173a14863d81961ffdc0f85afd5b776f3e0615
leemirim <leemirim9654@gmail.com>
2019-07-11 23:37:40 +0900

    Folder 모델 수정 및 마이그레이션

--- Molink/folders/admin.py
+++ Molink/folders/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Folder)
+class FolderAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'color',
+        'parent_folder',
+        'creator',
+    )
--- Molink/folders/models.py
+++ Molink/folders/models.py
@@ -1,3 +1,33 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    """ Base Model """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Folder(TimeStampedModel):
+    """ Track Model """
+    name = models.CharField(max_length=40)
+    color = models.CharField(max_length=20)
+    parent_folder = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE, 
+        null=True,
+        blank=True,
+        related_name='child_folders'
+        )
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='folders'        
+    )
+    
+    def __str__(self):
+        return f'{self.name}'
