a84fe06b3d7641f281667b17c64bc4a23f6533aa
leemirim <leemirim9654@gmail.com>
2019-08-12 21:01:31 +0900

    Category 모델 수정

--- Molink/categories/models.py
+++ Molink/categories/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from folders.models import TimeStampedModel
 
+
 class Category(TimeStampedModel):
-    """ Category Model """
-    name = models.CharField(max_length=50)
-    img_url = models.CharField(max_length=255)
+	""" Category Model """
+	name = models.CharField(max_length=50)
+	img_url = models.CharField(max_length=255)
