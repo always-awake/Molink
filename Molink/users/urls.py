4e16fe1296c71db2e7e53767fdfa6349195bb56b
leemirim <leemirim9654@gmail.com>
2019-08-19 14:24:33 +0900

    [#6][ADD] 유저 로그인 구현

--- Molink/users/urls.py
+++ Molink/users/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'users'
 urlpatterns = [
 	path('signup', views.UserSignUp.as_view()),
+	path('login', views.UserLogin.as_view()),
 ]
--- Molink/users/views.py
+++ Molink/users/views.py
@@ -1,27 +1,33 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.contrib.auth import login as django_login, logout as django_logout
+
 
 from . import serializers
-from . import models
+from .models import User
 
 
 class UserSignUp(APIView):
 
 	# 회원가입
 	def post(self, request, format=None):
-		password1 = request.POST['password1']
-		password2 = request.POST['password2']
+		username = request.POST.get("username", 'username_error')
+		password1 = request.POST.get("password1", 'password_error1')
+		password2 = request.POST.get("password2", 'password_error2')
 		if password1 != password2:
 			return Response(status=status.HTTP_400_BAD_REQUEST)
 		elif password1 == password2:
 			serializer = serializers.UserSignUpSerializer(data=request.data)
 			if serializer.is_valid():
 				serializer.save(password=password1)
+				user = User.objects.get(username=username, password=password1)
+				if user:
+					django_login(request, user)
+					print("로그인됨")
 				return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 			else:
 				return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 
 class UserProfile(APIView):
@@ -33,13 +39,18 @@ class UserProfile(APIView):
 	def put(self, request, format=None):
 		pass
 
-class UserLoing(APIView):
+
+class UserLogin(APIView):
 	# 유저 로그인
-	def post(self, request, format=None):
-		pass
+	def get(self, request, format=None):
+		username = request.POST.get("username", 'username_error')
+		password = request.POST.get("password", 'password_error2')
+		user = User.objects.get(username=username, password=password)
+		if user:
+			django_login(request, user)
+			return Response(status=status.HTTP_200_OK)
+		else:
+			return Response(status=status.HTTP_404_NOT_FOUND)
 
 
-class UserLogout(APIView):
-	# 유저 로그아웃
-	def post(self, reqeust, format=None):
-		pass
+
